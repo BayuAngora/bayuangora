ff2415d17e3c17528cb89ef395a9d6d5c3fc9d24
Bayu Angora <bayu.angora@gmail.com>
2022-11-10 18:16:34 +0700

    Create userway.js

---
+++ static/script/userway.js
@@ -0,0 +1,7 @@
+<script>
+(function(){var s = document.createElement("script");s.setAttribute("data-account","DtuD8NSmhd");s.setAttribute("src","https://cdn.userway.org/widget.js");document.body.appendChild(s);})();
+</script>
+
+<script>
+(function(d){var s = d.createElement("script");s.setAttribute("data-account", "DtuD8NSmhd");s.setAttribute("src", "https://accessibilityserver.org/widget.js");(d.body || d.head).appendChild(s);})(document)
+</script>
