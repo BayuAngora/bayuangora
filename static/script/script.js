7b07086d8e9e483fb023cfdbe40a47a615d46deb
Bayu Angora <bayu.angora@gmail.com>
2024-04-04 19:24:45 +0700

    Update script.js

--- static/script/script.js
+++ static/script/script.js
@@ -11,7 +11,7 @@ localStorage.removeItem("preferredTheme")),document.body.classList.toggle("mode"
 localStorage.getItem("preferredTheme")&&setMode(!0),document.getElementById("dark").addEventListener("click",(
 function(){setMode(!0)})),document.getElementById("light").addEventListener("click",(function(){setMode(!1)}));
 
-const share = document.querySelector(".share");
+const share = document.getElementById("share");
 share.addEventListener("click", event => {
 if (navigator.share) { navigator.share({
 title: "Bayu Angora", url: "" }).then(() => {
