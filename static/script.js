83fb02a85d23a0ffa716a7bb8a6fe508f0d985d3
Bayu Angora <16639529+BayuAngora@users.noreply.github.com>
2026-08-20 19:44:46 +0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-navigator.serviceWorker.register("/service.js");
+navigator.serviceWorker.register("/static/service.js");
 navigator.serviceWorker.ready.then(async function (registration) {
 if ("sync" in registration) {registration.sync.register("sync").catch(
 function (err) {console.warn("Background sync is disabled."); });} try {
